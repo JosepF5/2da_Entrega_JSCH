c97b692e2730f200aa19339994577ae757a13e5c
Josep Palomino <jfpalomino.22@gmail.com>
2021-11-16 21:13:00 -0500

    Jquery v1

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -60,53 +60,24 @@ class Producto{
 }
 //------------------------------COMPRAR--------------------------------------
 function loadCompra(arrGcompraz){
-    const contenedor=document.createElement("div")
-    contenedor.classList.add('bg-dark') 
-    const texto = document.getElementById('texto')
-    const h3 = document.getElementsByTagName('h3')[0]
-    h3.className = "titulo"
-    h3.innerText = "Ofertas por el mes de Halloween"
-    contenedor.append(h3)
-    contenedor.append(h3)
-    contenedor.append(h3)
+    const texto = $(`#texto`)
+    const h3 = $(`h3`)
+    h3.addClass('titulo')
+    h3.append("Ofertas por el mes de Halloween")
     texto.append(h3)
-    const contenedorProductos = document.getElementById('productos')
     //INTERACCION CON HTML
     if(arrGcompraz!=null){
         arrGcompraz.forEach( (prod) => {
-            const div = document.createElement('div')
-            div.id = `producto${prod.nombre}`
-            // TEMPLATE STRING
-            div.innerHTML = `
+            $('#productos').append(`
                 <h3 class="titulo">${prod.nombre}</h3>
                 <p></p>
                 <p>Precio: $${prod.precio}</p>
                 <p>Stock: ${prod.stock}</p>
-            `
-            contenedorProductos.append(div)
+                <p>Creada el: ${ new Date().toLocaleString() }</p>
+                <br>
+            `)
         })
     }
-    /*
-    arrGcompraz.forEach( (prod) => {
-        const div = document.createElement('div')
-        div.id = `producto${prod.nombre}`
-        // TEMPLATE STRING
-        div.innerHTML = `
-            <h3 class="titulo">${prod.nombre}</h3>
-            <p></p>
-            <p>Precio: $${prod.precio}</p>
-            <p>Stock: ${prod.stock}</p>
-        `
-        contenedorProductos.append(div)
-    })
-    */
-    /*
-    const input = document.getElementById('input')
-    console.log(input.value)
-    */
-    console.log("REGISTRO")
-    console.log(arrGcompraz)
-    console.log(compras)
     for (let compra of compras) {
         stringCompras+=compra.nombre+" a "+compra.mostrarPrecio(compra.stock)+" soles."+"\n"
         totalPrecio+=parseFloat(compra.mostrarPrecio(compra.stock))
@@ -148,86 +119,20 @@ function doCompra(){
         }
     }
     let arrGcompraz=[]
-    console.log(localStorage.getItem('Compras')+" Hola")
     Gcompras=localStorage.getItem('Compras') //Guardar el arreglo provisionalmente en string
     arrGcompraz=JSON.parse(Gcompras) //Conventirlo el string a arreglo 
-    /*
-    if(arrGcompras!=null){
-        console.log(arrGcompras)
-        for (let producto of compras) {
-            arrGcompras.push(producto)
-            console.log(arrGcompras)
-        }
-    }
-    */
     //Guardar en el nuevo arreglo los objetos de la nueva consulta
-    console.log(arrGcompraz)
-        for (let producto of compras) {
-            arrGcompraz.push(producto)
-        }
-    /*
-    arrGcompras.forEach( (prod) => {
-        const div = document.createElement('div')
-        div.id = `producto${prod.nombre}`
-        // TEMPLATE STRING
-        div.innerHTML = `
-            <h3 class="titulo">${prod.nombre}</h3>
-            <p></p>
-            <p>Precio: $${prod.precio}</p>
-            <p>Stock: ${prod.stock}</p>
-        `
-        contenedorProductos.append(div)
-    })
-    */
+    for (let producto of compras) {
+        arrGcompraz.push(producto)
+    }
     //Setearlo en el local storage
     localStorage.setItem('Compras',JSON.stringify(arrGcompraz))
-    
-    console.log(Gcompras)
-    console.log(arrGcompras)
-    console.log(arrGcompraz)
-    console.log(localStorage.getItem('Compras'))
-    console.log(typeof(Gcompras))
-    console.log(typeof(arrGcompras))
-    console.log(typeof(arrGcompraz))
-    console.log(typeof(Tcompras))
-    /*
-    arrGcompras.forEach( (prod) => {
-        const div = document.createElement('div')
-        div.id = `producto${prod.nombre}`
-        // TEMPLATE STRING
-        div.innerHTML = `
-            <h3 class="titulo">${prod.nombre}</h3>
-            <p></p>
-            <p>Precio: $${prod.precio}</p>
-            <p>Stock: ${prod.stock}</p>
-        `
-    })
-    */
     loadCompra(arrGcompraz);
 }
 //------------------------------Reporte-PreCargado------------------------------
-/*
-const tituloPrincipal = document.getElementsByTagName('h1')[0]
-tituloPrincipal.innerHTML = "Desafio <strong> 09 </strong> | Eventos"
-*/
-/*
-const titulo = document.getElementById('titulo')
-titulo.innerText = "Bienvenido nuevamente! :)"
-titulo.className = "gigante titulo"
-*/
-const items = document.getElementsByClassName('item')
-for (const item of items) {
-    item.classList.add('gigante') 
-}
 //EVENTO COMPRAR
-const boton=document.getElementById('btnComprar')
-boton.addEventListener("click",comprarClick)
-function comprarClick(){
+const boton = $(`#btnComprar`)
+boton.click( () => {
     flag=0;
     doCompra();
-}
-//------------------------------Llamado de datos de ingreso------------------------------
-//--Proximamente
-//------------------------------Reporte-Final------------------------------
-//--Proximamente
-
+})

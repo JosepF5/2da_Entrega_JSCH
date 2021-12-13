ed3059031d05fca10444605fc35ce29a6d49a852
Josep Palomino <jfpalomino.22@gmail.com>
2021-12-13 15:46:03 -0500

    UpdateCarritoCompra_v5

--- js/miarchivo_v2.js
+++ js/miarchivo_v2.js
@@ -1,6 +1,6 @@
 //------------------------------Declaracion de variables------------------------------
 let productos=[]
-/*
+
 const cargarDatos = async () => {
     const resp = await fetch('..//js/productos.json')
     const data = await resp.json()
@@ -11,13 +11,12 @@ const cargarDatos = async () => {
 }
 
 cargarDatos()
-*/
+/*
 $.get('..//js/productos.json', (resp) => {
     const data=resp
     productos = data
-    console.log(productos)
     mostrarProductos(productos)
-})
+})*/
 let cantidad
 let compras=[]
 let Tcompras=[]
@@ -52,7 +51,11 @@ function mostrarProductos(array) {
                 <div class="col-md-8">
                   <div class="card-body ">
                     <h5 class="card-title titulo__Cartas">${prod.nombre}</h5>
-                    <p class="card-text"><small class="text-muted">$${prod.precioG}</small></p>
+                    <p class="card-text"><small>$${prod.precioG}</small></p>
+                    <p class="card-text"><small class="text-muted">Stock: ${prod.stockG}</small></p>
+                    <button type="button" class="botonUpdateAmount btn btn-secondary" onclick=plusAmount(${prod.id})>+</button>
+                    <span id="cantidadProducto${prod.id}">1</span>
+                    <button type="button" class="botonUpdateAmount btn btn-secondary" onclick=minusAmount(${prod.id})>-</button>
                     <button type="button" class="btn btn-warning" onclick=agregarAlCarrito(${prod.id})>Agregar al Carrito</button>
                   </div>
                 </div>
@@ -62,7 +65,22 @@ function mostrarProductos(array) {
         `)
     })
 }
-//------------------------------Definicion de clase "Producto"------------------------------
+//------------------------------Aumentar-Disminuir Monto de Productos------------------------------
+function plusAmount(id) {
+    const cantidad = document.getElementById('cantidadProducto'+id)
+    let producto = productos.find( el => el.id == id )
+    if(cantidad.innerText < producto.stockG){
+        cantidad.innerText = parseInt(cantidad.innerText) + 1
+    }
+}
+function minusAmount(id) {
+    const cantidad = document.getElementById('cantidadProducto'+id)
+    if(cantidad.innerText > 1){
+        cantidad.innerText = parseInt(cantidad.innerText) - 1
+    }
+    
+}
+//------------------------------CRUD CARRITO DE COMPRA------------------------------
 
 function agregarAlCarrito(itemId) {
 
@@ -72,20 +90,20 @@ function agregarAlCarrito(itemId) {
         itemEnCarrito.cantidad += 1
     } else {
         let {id, nombre, precioG} = productos.find( el => el.id == itemId )
-        carrito.push({id: id, nombre: nombre, precioG: precioG, stockG: 1})
+        const cantidad = document.getElementById('cantidadProducto'+id)
+        carrito.push({id: id, nombre: nombre, precioG: precioG, stockG: parseInt(cantidad.innerText)})
+        let producto = productos.find( el => el.id == itemId )
+        console.log(producto.stockG)
+        producto.stockG -= parseInt(cantidad.innerText)
+        console.log(producto.stockG)
+        cantidad.innerText = 1
     }
-
-
-
     actualizarCarrito()
 }
 
 function eliminarProducto(id) {
     let productoAEliminar = carrito.find( el => el.id == id )
-    console.log(id)
-    console.log(productoAEliminar)
     productoAEliminar.stockG--
-    console.log(productoAEliminar.stockG)
     if (productoAEliminar.stockG <= 0) {
         let indice = carrito.indexOf(productoAEliminar)
         carrito.splice(indice, 1)
@@ -119,9 +137,3 @@ function actualizarCarrito() {
 }
 
 //------------------------------Reporte-PreCargado------------------------------
-//EVENTO COMPRAR
-const boton = $(`#btnComprar`)
-boton.click( () => {
-    flag=0;
-    doCompra();
-})
